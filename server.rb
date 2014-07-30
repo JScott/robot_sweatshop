05cc2603e3d9374fb15aec7a7a33eefdd414ffd6
Justin Scott (via UET AWS) <justin.scott1@telus.com>
2014-07-30 17:45:19 +0000

    :snail: minor typo

--- server.rb
+++ server.rb
@@ -24,7 +24,7 @@ def git_push_url(bitbucket_data, job_name)
     "GIT_AUTHOR=#{commit['raw_author']}",
     "GIT_HASH=#{commit['raw_node']}",
     "GIT_MESSAGE=#{commit['message']}",
-    "GIT_REPO=#{repo['absolute_url']}".
+    "GIT_REPO=#{repo['absolute_url']}",
     "GIT_BRANCH=#{commit['branch']}",
     "GIT_SOURCE_URL=#{source_url}"
   ].join '&'
