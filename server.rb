5243c06a3568347fbb2e7a834370f7ed3ba42523
Justin Scott (via UET AWS) <justin.scott1@telus.com>
2014-07-30 17:09:17 +0000

    Multiple authors and cleaner code

--- server.rb
+++ server.rb
@@ -15,21 +15,30 @@ get '/' do
   RDiscount.new(markdown).to_html
 end
 
-def git_push_url(bitbucket_data)
+def git_push_url(bitbucket_data, job_name)
+  authors = []
+  bitbucket_data['commits'].each do |commit|
+    authors.push commit['raw_author']
+  end
   parameters = [
     "token=#{CONF['token']}",
-    "GIT_AUTHOR=#{bitbucket_data['commits'].last['raw_author']}",
+    "GIT_AUTHOR=#{authors.uniq.join ', '}",
     "GIT_HASH=#{bitbucket_data['commits'].last['raw_node']}",
     "GIT_MESSAGE=#{bitbucket_data['commits'].last['message']}",
     "GIT_REPO=#{bitbucket_data['repository']['absolute_url']}"
   ].join '&'
-  return URI.escape "http://#{CONF['host']}:#{CONF['port']}/job/#{params['job']}/buildWithParameters?#{parameters}"
+  puts parameters
+  URI.escape "http://#{CONF['host']}:#{CONF['port']}/job/#{job_name}/buildWithParameters?#{parameters}"
 end
 
-post '/bitbucket-payload-for/:job' do
+post '/:tool/payload-for/:job' do
   request.body.rewind
-  git_data = URI.decode_www_form(request.body.read)[0][1]
-  git_data = JSON.parse git_data
-  puts git_push_url(git_data)
-  HTTParty.get git_push_url(git_data)
+  case params['tool']
+  when 'bitbucket'
+    git_data = URI.decode_www_form(request.body.read)[0][1]
+    git_data = JSON.parse git_data
+    HTTParty.get git_push_url(git_data, params['job'])
+  else
+    puts 'Error: unknown tool called'
+  end
 end
