45e16457e76814ea3fab3b8a8dae3d7b866f1d06
Justin Scott (via UET AWS) <justin.scott1@telus.com>
2014-08-11 20:42:22 +0000

    Upgraded server script
    Modularized code
    Skeleton to run scripts

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'http://rubygems.org'
 gem 'sinatra', '~> 1.4'
 gem 'rdiscount', '~> 2.1'
-gem 'httparty', '~> 0.13'
 gem 'thin', '~> 1.6'
 gem 'rspec', '~> 3.0'
--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+require 'yaml'
 require 'sinatra'
 require 'rdiscount'
 require_relative 'parse/bitbucket'
@@ -6,25 +7,50 @@ require_relative 'parse/bitbucket'
 set :port, 6381
 set :bind, '0.0.0.0'
 
-conf = YAML.load_file('config.yaml')
-conf.each do |key, value|
-  set key.to_sym, value
-end
+CONF = YAML.load_file('config.yaml')
 
 get '/' do
   markdown = File.read 'README'
   RDiscount.new(markdown).to_html
 end
 
-post '/:tool/payload-for/:job' do
-  request.body.rewind
-  payload = case params['tool']
+def parser_for(tool)
+  case tool
   when 'bitbucket'
-    BitbucketPayload.new request.body.read
+    BitbucketPayload
   else
-    ''
+    nil
   end
-  raise "ERROR: no parser for #{params['tool']} payload for #{params['job']}" if payload.empty?
+end
 
-  
+def parse_payload(tool, data)
+  parse = parser_for params['tool']
+  parse.new data
+end
+
+def verified(payload)
+  if payload.nil?
+    puts "Stopping. No parser for #{params['tool']}"
+    return false
+  end
+
+  unless CONF['branches'].include? payload.branch
+    puts "Stopping. '#{payload.branch}' is not in our branch watchlist:", "#{CONF['branches']}"
+    return false
+  end
+
+  true
+end
+
+def run_scripts_with(payload)
+  CONF['scripts'].each do |path|
+    puts "Running #{File.expand_path path}..."
+  end
+end
+
+post '/:tool/payload-for/:job' do
+  puts "Received #{params['tool']} payload for #{params['job']}"
+  request.body.rewind
+  payload = parse_payload params['tool'], request.body.read
+  run_scripts_with payload if verified payload
 end
