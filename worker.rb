01e1d42557aa86f020432e4d523f35395aa17a4f
Justin Scott (via UET AWS) <justin.scott1@telus.com>
2014-08-26 18:35:19 +0000

    Changed sidekiq configuration

--- worker.rb
+++ worker.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
 file_path = File.expand_path File.dirname(__FILE__)
-IO.popen("sidekiq --require #{file_path}/server.rb") do |io|
+command = [
+  "sidekiq",
+  "--require #{file_path}/server.rb",
+  "--config #{file_path}/sidekiq.yaml"
+]
+IO.popen("#{command.join ' '}") do |io|
   while line = io.gets
     puts line
   end
