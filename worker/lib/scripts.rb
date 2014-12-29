7765a9872e52774a7e5a45e023e9495cf9b15749
JScott <jvscott@gmail.com>
2014-12-29 16:58:39 -0500

    Nuts to loggers, let's just abuse STDOUT and pipe it

--- worker/lib/scripts.rb
+++ worker/lib/scripts.rb
@@ -1,9 +1,9 @@
 require 'logger'
 require 'fileutils'
 
-def from_workspace(job, with_logger: Logger.new(STDOUT))
+def from_workspace(job)
   path = workspace_path_for job
-  with_logger.info "Working from '#{path}' for '#{job}'"
+  puts "Working from '#{path}' for '#{job}'"
   FileUtils.mkdir_p path
   Dir.chdir path do
     yield
@@ -15,21 +15,21 @@ def workspace_path_for(job_name)
   "#{current_dir}/../workspaces/#{job_name}"
 end
 
-def work_on(path, with_logger: Logger.new(STDOUT))
-  with_logger.info "Running '#{path}'..."
+def work_on(path)
+  puts "Running '#{path}'..."
   # TODO: path.split(' ') to bypass the shell when we're not using env vars
   IO.popen(path) do |io|
     while line = io.gets
-      with_logger.info line
+      puts line
     end
   end
-  with_logger.info "Script done. (exit status: #{$?.exitstatus})"
+  puts "Script done. (exit status: #{$?.exitstatus})"
 end
 
-def start_job(job_name, scripts, with_logger: Logger.new(STDOUT))
-  from_workspace(job_name, with_logger: with_logger) do
+def start_job(job_name, scripts)
+  from_workspace(job_name) do
     scripts.each do |command|
-      work_on command, with_logger: with_logger
+      work_on command
     end
   end
 end
--- worker/spec/script_running_spec.rb
+++ worker/spec/script_running_spec.rb
@@ -1,29 +1,24 @@
 require_relative '../lib/scripts'
 require 'rspec/mocks'
 
-describe 'work_on' do
-  it 'logs debug info' do
-    expect_any_instance_of(Logger).to receive(:info).with(/Running/)
-    expect_any_instance_of(Logger).to receive(:info).with(/1/)
-    expect_any_instance_of(Logger).to receive(:info).with(/exit status/)
-    work_on 'echo 1'
+describe 'lib/scripts' do
+  describe 'work_on' do
+    it 'prints scrpit output to stdout' do
+      command = 'echo 1'
+      expect { work_on command }.to output(/1/).to_stdout
+      expect { work_on command }.to output(/exit status/).to_stdout
+    end
   end
-  
-  it 'can take a custom logger' do
-    custom_logger = Logger.new STDERR
-    expect(custom_logger).to receive(:info).at_least(:once)
-    work_on 'echo 1', with_logger: custom_logger
-  end
-end
 
-describe 'start_job' do
-  before(:context) do
-    @test_job = 'test-job'
-    @scripts = ['echo 1']
-  end
-  
-  it 'works out of a job-specific workspace' do
-    expect(Dir).to receive(:chdir).with(/workspaces\/#{@test_job}/)
-    start_job @test_job, @scripts
+  describe 'start_job' do
+    before(:context) do
+      @test_job = 'test-job'
+      @scripts = ['echo 1']
+    end
+
+    it 'works out of a job-specific workspace' do
+      expect(Dir).to receive(:chdir).with(/workspaces\/#{@test_job}/)
+      start_job @test_job, @scripts
+    end
   end
 end
