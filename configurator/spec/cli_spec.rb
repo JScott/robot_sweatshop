830ea96a969d8e745e6344b703b4aa20e080c631
JScott <jvscott@gmail.com>
2014-12-31 14:14:43 -0500

    :snail: missed @

---
+++ configurator/spec/cli_spec.rb
@@ -0,0 +1,27 @@
+require 'fileutils'
+require 'yaml'
+
+describe 'configurator', 'sweatshop' do
+  describe 'job' do
+    before(:context) do
+      @git_repo = 'git@github.com:ruby/ruby.git'
+      @job_path = "#{__dir__}/../../conductor/jobs/ruby.yaml"
+    end
+    
+    after(:each) do
+      FileUtils.rm @job_path
+    end
+    
+    it 'creates a job configuration from a given git repo' do
+      `sweatshop manage #{@git_repo}`
+      expect(File.file? @job_path).to be_true
+      job = YAML.load_file @job_path
+      ['branches', 'scripts', 'environment'].each do |key|
+        expect(job).to have_key key
+      end
+    end
+  end
+end
+
+describe 'configurator', 'sweatshop-worker' do
+end
