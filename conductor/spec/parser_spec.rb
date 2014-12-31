9a996ab156d40b7dac15def315a90ada58d46ece
JScott <jvscott@gmail.com>
2014-12-30 19:00:14 -0500

    Conductor parser_for spec

---
+++ conductor/spec/parser_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/payload'
+
+describe 'conductor', 'parser_for' do
+  it 'returns a payload parser for bitbucket' do
+    expect(parser_for 'bitbucket').to be BitbucketPayload
+  end
+  
+  it 'returns nil for an unrecognized tool' do
+    expect(parser_for 'whatever').to be_nil
+  end
+end
