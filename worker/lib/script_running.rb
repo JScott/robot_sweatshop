3c77888884c239e3d14950672c866118716b9311
JScott <jvscott@gmail.com>
2015-01-04 13:23:22 -0500

    A bit more worker refactoring

--- worker/lib/script_running.rb
+++ worker/lib/script_running.rb
@@ -2,21 +2,20 @@ require 'logger'
 require 'fileutils'
 
 def from_workspace(job_name)
-  path = workspace_path_for job_name
+  path = workspace_path for_job: job_name
   FileUtils.mkpath path
   Dir.chdir(path) { yield if block_given? }
 end
 
-def workspace_path_for(job_name)
-  current_dir = File.expand_path File.dirname(__FILE__)
-  "#{current_dir}/../workspaces/#{job_name}"
+def workspace_path(for_job:)
+  File.expand_path "#{__dir__}/../workspaces/#{for_job}"
 end
 
 def run(command, log: Logger.new(STDOUT))
   log.info "Running '#{command}'..."
   # TODO: path.split(' ') to bypass the shell when we're not using env vars
   IO.popen(command) do |io|
-    while line = io.gets
+    while line = io.gets do
       log.info line
     end
   end
