b71b7240ed615e187c896a094eeb4a13961ecd8a
JScott <jvscott@gmail.com>
2015-01-05 00:25:07 -0500

    Fixed conductor tests

--- conductor/spec/payload_spec.rb
+++ conductor/spec/payload_spec.rb
@@ -7,22 +7,28 @@ describe 'conductor', 'payload' do
     @payload_data = YAML.load_file payload_path
   end
 
-  context 'with a malformed bitbucket payload' do
+  context 'with a valid bitbucket payload' do
     before(:context) do
       @payload = BitbucketPayload.new @payload_data['bitbucket']
     end
 
     it 'parses git commit data' do
+      expected_author = {
+        'name' => 'Marcus Bertrand',
+        'email' => 'marcus@somedomain.com',
+        'username' => 'marcus'
+      }      
       expect(@payload.repo_slug).to eq 'marcus/project-x'
       expect(@payload.source_url).to eq 'https://bitbucket.org/marcus/project-x/commits/620ade18607ac42d872b568bb92acaa9a28620e9/?at=master'
-      expect(@payload.author).to eq 'Marcus Bertrand <marcus@somedomain.com>'
+      expect(@payload.clone_url).to eq 'https://bitbucket.org/marcus/project-x/'
+      expect(@payload.author).to eq expected_author
       expect(@payload.hash).to eq '620ade18607ac42d872b568bb92acaa9a28620e9'
       expect(@payload.branch).to eq 'master'
       expect(@payload.message).to eq "Added some more things to somefile.py\n"
     end
 
     it 'can return the git commit data in a hash' do
-      expect(@payload.git_commit_data.size).to eq(6)
+      expect(@payload.git_commit_data.size).to eq 7
     end
   end
 
