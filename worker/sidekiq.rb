b112ddc0c4f448bcb2432fd1d6753ae14109fc72
JScott <jvscott@gmail.com>
2015-01-06 13:27:34 -0500

    Pulled parser out of conductor

--- worker/sidekiq.rb
+++ worker/sidekiq.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
-
-file_path = File.expand_path File.dirname(__FILE__)
 command = [
   '/usr/local/bin/sidekiq',
-  "--require #{file_path}/lib/queuing.rb",
-  "--config #{file_path}/sidekiq.yaml"
+  "--require #{__dir__}/lib/queuing.rb",
+  "--config #{__dir__}/sidekiq.yaml"
 ]
 IO.popen("#{command.join ' '}") do |io|
   while line = io.gets
