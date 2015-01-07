e71312abcf660829fa8327d7994be477862c3e0f
JScott <jvscott@gmail.com>
2015-01-06 22:30:19 -0500

    cuke creates thread again, mistakenly added that

--- features/support/processes.rb
+++ features/support/processes.rb
@@ -3,14 +3,14 @@ require 'ezmq'
 Before('@run_queue_handler') do
   unless $queue_handler_running
     script = File.expand_path "#{__dir__}/../../queue/handler.rb"
-    #$queue_handler = Thread.new { `#{script}` } 
+    $queue_handler = Thread.new { `#{script}` } 
     $client = EZMQ::Client.new
     $queue_handler_running = true
   end
 end
 
 After('@run_queue_handler') do
-  #Thread.kill $queue_handler while $queue_handler.alive?
+  Thread.kill $queue_handler while $queue_handler.alive?
 end
 
 at_exit do
