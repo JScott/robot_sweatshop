d9f8624f814c5374b19c878a4a4049aab295b80b
JScott <jvscott@gmail.com>
2015-01-06 19:38:22 -0500

    Moneta should only write to where it's supposed to now

--- queue/lib/file-queue.rb
+++ queue/lib/file-queue.rb
@@ -4,8 +4,10 @@ require 'moneta'
 class FileQueue
   attr_accessor :queue
   
+  MONETA_DIR = File.expand_path "#{__dir__}/moneta"
+  
   def initialize(name)
-    store = Moneta.new :File, dir: 'moneta'
+    store = Moneta.new :File, dir: MONETA_DIR
     store[name] = []
     @queue = store[name]
   end
