ea697ec5fb08866d32c90a2ac9674892711aed9d
JScott <jvscott@gmail.com>
2015-01-08 21:27:21 -0500

    No need for testing to have its own moneta instances

--- features/step_definitions/file_queue.rb
+++ features/step_definitions/file_queue.rb
@@ -1,9 +1,16 @@
 require 'moneta'
+require_relative '../../queue/lib/file-queue.rb'
 
 Given /^nothing is in the '(.*?)' queue$/ do |queue|
-  $queues.delete queue
+  queue = FileQueue.new queue
+  #queue.delete queue
+  expect(queue.size).to eq 0
 end
 
 Given /^something is in the '(.*?)' queue$/ do |queue|
-  $queues[queue] = ['one', 'two']
+  queue = FileQueue.new queue
+  unless queue.size > 0
+    queue.push 'one'
+    queue.push 'two'
+  end
 end
--- features/support/queue.rb
+++
@@ -1,9 +0,0 @@
-require 'moneta'
-
-Before do
-  unless $queue_storage_opened
-    moneta_dir = File.expand_path "#{__dir__}/../../queue/lib/moneta"
-    $queues = Moneta.new :File, dir: moneta_dir
-    $queue_storage_opened = true
-  end
-end
