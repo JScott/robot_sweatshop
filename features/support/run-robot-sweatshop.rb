c5f4254a4f6c22d5d4df8815c572cb38c145b2ad
JScott <jvscott@gmail.com>
2015-01-09 00:05:31 -0500

    cucumber sweatshop startup now uses bluepill instead of hax

--- features/support/run-robot-sweatshop.rb
+++ features/support/run-robot-sweatshop.rb
@@ -1,13 +1,3 @@
-$scripts = [
-  'queue/handler.rb',
-  'queue/broadcaster.rb'
-]
-
-$scripts.map! { |path| File.expand_path "#{__dir__}/../../#{path}" }
-$scripts.map! { |path| Process.spawn path }
-
-at_exit do
-  $scripts.each do |pid|
-    Process.kill "TERM", pid
-  end
-end
+script = "#{__dir__}/../../robot-sweatshop.rb"
+puts `#{script} init`
+puts `#{script} start`
