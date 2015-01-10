d565acc47bb597afb24efb54b1bd3be65d79a936
JScott <jvscott@gmail.com>
2015-01-09 19:09:20 -0500

    sudo still kicking around. Broken right now without sudo though

--- robot-sweatshop.rb
+++ robot-sweatshop.rb
@@ -7,7 +7,7 @@ end
 
 if ARGV[0] == 'init'
   config = File.expand_path "#{__dir__}/robot-sweatshop.pill"
-  exec "sudo bluepill load #{config} --no-privileged"
+  exec "bluepill load #{config} --no-privileged"
 else
-  exec "sudo bluepill robot-sweatshop #{ARGV[0]} --no-privileged"
+  exec "bluepill robot-sweatshop #{ARGV[0]} --no-privileged"
 end
