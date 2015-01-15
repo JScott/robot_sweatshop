ced617bd58cb92f7912126a3ad8dab085240436c
JScott <jvscott@gmail.com>
2015-01-15 13:13:53 -0500

    Added queue mirroring for testing purposes

--- queue/lib/file-queue.rb
+++ queue/lib/file-queue.rb
@@ -5,25 +5,34 @@ class FileQueue
   attr_accessor :store
 
   MONETA_DIR = File.expand_path "#{__dir__}/moneta"
+  MIRRORING_ENABLED = 'enable-mirroring'
   
   def initialize(name)
     @name = name
+    @mirror_name = "mirror-#{name}"
     @store ||= Moneta.new :File, dir: MONETA_DIR
     @store[name] ||= []
   end
+  
+  def self.mirroring=(boolean)
+    @store ||= Moneta.new :File, dir: MONETA_DIR
+    @store[MIRRORING_ENABLED] = boolean
+  end
 
   def push(item)
+    @store[@mirror_name] = @store[@mirror_name].push item if @store[MIRRORING_ENABLED]
     @store[@name] = @store[@name].push item
   end
   
-  def pop
-    item = @store[@name].first
-    @store[@name] = @store[@name][1..-1]
+  def pop(from: :store)
+    name =  from == :mirror ? @mirror_name : @name
+    item = @store[name].first
+    @store[name] = @store[name][1..-1]
     item
   end
 
   def size
-    # TODO: sometimes calling nil.size ...
+    # TODO: sometimes calling nil.size when you push empty into the queue ...
     @store[@name].size
   end
 
