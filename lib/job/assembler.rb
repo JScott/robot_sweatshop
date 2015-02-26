a858b3e122c751de5cdfb733e7282b07937fb6cd
JScott <jvscott@gmail.com>
2015-02-26 12:20:32 -0800

    Added new queue-helper abstraction to job assembler

--- lib/job/assembler.rb
+++ lib/job/assembler.rb
@@ -1,57 +1,35 @@
 #!/usr/bin/env ruby
-require 'ezmq'
-require 'json'
 require 'yaml'
+require_relative '../queue-helper'
 
 JOB_DIR = "#{__dir__}/../../jobs"
-
-@client = EZMQ::Client.new port: 5556
-@input_queue = 'parsed-payload'
-@output_queue = 'jobs'
 
 def assemble_job(parsed)
   job_config_path = "#{JOB_DIR}/#{parsed['job_name']}.yaml"
   return nil unless File.file? job_config_path
   job_config = YAML.load_file job_config_path
-  branch_whitelisted = job_config['branch_whitelist'].include? parsed['payload']['branch']
-  valid_payload = parsed['payload'].class == Hash
-  if branch_whitelisted && valid_payload
+
+  issues = []
+  if not job_config['branch_whitelist'].include? parsed['payload']['branch']
+    issues.push "Branch '#{parsed['payload']['branch']}' not whitelisted"
+  elsif parsed['payload'].class != Hash
+    issues.push "Invalid payload"
+  end
+
+  if issues.empty?
     {
       commands: job_config['commands'],
       context: job_config['environment'].merge(parsed['payload'])
     }
   else
+    puts "Dropping job:"
+    issues.each { |issue| puts "  #{issue}" }
     nil
   end
 end
 
-def queue(assembled_job)
-  @client.request "#{@output_queue} #{JSON.generate assembled_job}"
-end
-
-def dequeue
-  @client.request @input_queue
-end
-
-def wait_for_parsed_payload
-  subscriber = EZMQ::Subscriber.new port: 5557, topic: 'busy-queues'
-  subscriber.listen do |message|
-    if message == @input_queue
-      data = dequeue
-      yield data unless data.empty?
-    end
-  end
-end
-
-wait_for_parsed_payload do |data|
-  begin
-    data = JSON.parse data
-  rescue JSON::ParserError => e
-    data = nil
-  end
-  unless data.nil?
-    puts "Assembling: #{data}"
-    assembled_job = assemble_job data
-    queue assembled_job if assembled_job
-  end
+QueueHelper.wait_for_queue('parsed-payload') do |data|
+  puts "Assembling: #{data}"
+  assembled_job = assemble_job data
+  QueueHelper.enqueue assembled_job, to: 'jobs' if assembled_job
 end
