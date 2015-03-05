e4b5d9f0a90d06602630143a1b14e6d2ad660cd8
JScott <jvscott@gmail.com>
2015-03-05 00:19:45 -0800

    More clarity around which configuration is loaded

--- bin/lib/start.rb
+++ bin/lib/start.rb
@@ -4,7 +4,7 @@ def start_sweatshop(for_environment:)
   if $?.exitstatus != 0
     notify :failure, output
   else
-    notify :success, 'Robot Sweatshop loaded'
+    notify :success, "Robot Sweatshop loaded with a #{for_environment} configuration"
     notify :info, `eye restart robot_sweatshop`
     puts 'Check \'eye --help\' for more info on managing the processes'
   end
