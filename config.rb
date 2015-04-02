5fd6c5a42c1b70570ad58a38f724f201bb6ddb0a
JScott <jvscott@gmail.com>
2015-04-02 15:47:27 -0700

    Hierarchical configs with defaults

--- config.rb
+++ config.rb
@@ -1,14 +1,14 @@
 require 'configatron'
 require 'yaml'
 
-# TODO: namespace to avoid collisions?
-
 configurations = [
   "#{__dir__}/config.yaml",
-  "#{__dir__}/config.user.yaml",
-  "/etc/robot_sweatshop/config.yaml"
+  "/etc/robot_sweatshop/config.yaml",
+  "~/.robot_sweatshop/config.yaml",
+  ".robot_sweatshop/config.yaml",
 ]
 configurations.each do |config_path|
+  config_path = File.expand_path config_path
   if File.file? config_path
     hash = YAML.load_file config_path
     configatron.configure_from_hash hash
