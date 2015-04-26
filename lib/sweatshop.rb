ae135678aec784636159311c1d8fc0b7095a1a4c
JScott <jvscott@gmail.com>
2015-04-25 23:19:21 -0700

    Reworked payload parser in a big way, still needs work

--- lib/sweatshop.rb
+++ lib/sweatshop.rb
@@ -5,12 +5,11 @@ require_relative 'sweatshop/config'
 require_relative 'sweatshop/payload/payload'
 require_relative 'sweatshop/payload/bitbucket'
 require_relative 'sweatshop/payload/github'
+require_relative 'sweatshop/payload/json'
 
 require_relative 'sweatshop/cli/common'
 require_relative 'sweatshop/cli/config'
-require_relative 'sweatshop/cli/inspect'
 require_relative 'sweatshop/cli/job'
-require_relative 'sweatshop/cli/setup'
 require_relative 'sweatshop/cli/start'
 
 
