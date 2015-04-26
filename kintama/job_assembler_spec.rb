a0879e30fca7a59efedf2979b02c51e02720263f
JScott <jvscott@gmail.com>
2015-04-26 13:30:32 -0700

    Getting job assembler tests ready for using the new payload parser

--- kintama/job_assembler_spec.rb
+++ kintama/job_assembler_spec.rb
@@ -11,62 +11,55 @@ describe 'the Job Assembler' do
 
   setup do
     @client = EZMQ::Client.new port: 5556
-    @parsed_payloads_queue = 'parsed-payload'
+    @payloads_queue = 'payload'
     @jobs_queue = 'jobs'
     clear_all_queues
   end
 
-  given 'valid parsed payload data in \'parsed-payload\'' do
+  given 'valid payload data in \'payload\'' do
     setup do
       payload = example_parsed_payload(for_branch: 'develop')
-      @client.request "#{@parsed_payloads_queue} #{payload}"
+      @client.request "#{@payloads_queue} #{payload}"
       sleep $for_a_while
     end
 
-    should 'remove it from \'parsed-payload\'' do
-      response = @client.request @parsed_payloads_queue
+    should 'remove it from \'payload\'' do
+      response = @client.request @payloads_queue
       assert_equal '', response
     end
 
     should 'enqueue commands, context, and job name to \'jobs\'' do
       response = @client.request "mirror-#{@jobs_queue}"
-      response = JSON.parse response
+      response = JSON.load response
       assert_kind_of Hash, response['context']
       assert_kind_of Array, response['commands']
       assert_kind_of String, response['job_name']
     end
 
-    should 'convert objects into JSON strings' do
+    should 'only enqueue strings into context' do
       response = @client.request "mirror-#{@jobs_queue}"
-      response = JSON.parse response
-      assert_kind_of Hash, JSON.parse(response['context']['converts_to_string'])
-    end
-
-    should 'only enqueue string objects to context' do
-      response = @client.request "mirror-#{@jobs_queue}"
-      response = JSON.parse response
+      response = JSON.load response
       response['context'].each do |_key, value|
         assert_kind_of String, value
       end
     end
   end
 
-  given 'invalid job data in \'parsed-payload\'' do
+  given 'invalid job data in \'payload\'' do
     setup do
       invalid_data = {
         ignored_branch: example_parsed_payload(for_branch: 'not_on_whitelist'),
-        bad_payload:    example_parsed_payload(with_payload: 'not hash'),
         bad_job:        example_parsed_payload(for_job: 'asdf'),
         not_json:       'not_json'
       }
       invalid_data.each do |_type, datum|
-        @client.request "#{@parsed_payloads_queue} #{datum}"
+        @client.request "#{@payloads_queue} #{datum}"
       end
       sleep $for_a_while
     end
 
-    should 'remove all of it from \'parsed-payload\'' do
-      response = @client.request @parsed_payloads_queue
+    should 'remove all of it from \'payload\'' do
+      response = @client.request @payloads_queue
       assert_equal '', response
     end
 
