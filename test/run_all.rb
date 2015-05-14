2ac6dd56aa48cd0a297f7978394d69e8fe9ee47f
JScott <jvscott@gmail.com>
2015-05-13 18:00:23 -0700

    Committed to never running the tests together due to process initialization

--- test/run_all.rb
+++ test/run_all.rb
@@ -1,3 +1,15 @@
-Dir.glob("#{__dir__}/*_spec.rb").each do |spec_file|
-  require_relative spec_file
+# Because of needing to isolate processes, I can't run all tests in one call.
+# This means that I have multiple sets of results which isn't nice, but the
+#   tests are largely for the sake of TDD anyway.
+
+def run_test(component)
+  test_path = File.expand_path __dir__
+  IO.popen("ruby #{test_path}/#{component}_spec.rb") do |data|
+    while line = data.gets
+      puts line
+    end
+  end
 end
+
+run_test 'input'
+run_test 'conveyor'
