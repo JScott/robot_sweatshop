94371c57d21e382d6a903a7f39bc820eeaa73729
JScott <jvscott@gmail.com>
2015-05-13 17:01:32 -0700

    Proper tests and some clean up for Input

--- test/shared/helpers.rb
+++ test/shared/helpers.rb
@@ -14,52 +14,55 @@ module InputHelper
     payload_strings = YAML.load_file "#{__dir__}/../data/payload_data.yaml"
     payload_strings[of_format.downcase]
   end
+
   def input_http_url(for_job: 'test_job')
     "http://localhost:#{configatron.http_port}/payload-for/#{for_job}"
   end
-  def user_agent_for(format)
-    case format
-    when 'Bitbucket'
-      'Bitbucket.org'
-    when 'Github'
-      'Github-Hookshot'
-    else
-      'SomeUserAgent'
-    end
-  end
-  def example_payload_request(of_format:)
-    {
-      payload: example_raw_payload(of_format: of_format),
-      user_agent: user_agent_for(of_format)
-    }
-  end
-  def job_request_data(type)
-    case type
-    when 'Git'
-      ['Bitbucket', 'git_job'] # develop branch payload
-    when 'JSON'
-      ['JSON', 'test_job']
-    when 'MinimalJob'
-      ['JSON', 'minimal_job']
-    when 'IgnoredBranch'
-      ['Github', 'git_job'] # master branch payload
-    when 'UnknownJob'
-      ['Bitbucket', 'unknown_job']
-    when 'EmptyJob'
-      ['JSON', 'empty_job']
-    when 'NonJSON'
-      ['NonJSON', 'test_job']
-    else
-      ['', '']
-    end
-  end
-  def example_job_request(of_type:)
-    format, job_name = job_request_data of_type
-    Oj.dump payload: example_raw_payload(of_format: format),
-            job_name: job_name,
-            user_agent: user_agent_for(format)
-  end
+  # def user_agent_for(format)
+  #   case format
+  #   when 'Bitbucket'
+  #     'Bitbucket.org'
+  #   when 'Github'
+  #     'Github-Hookshot'
+  #   else
+  #     'SomeUserAgent'
+  #   end
+  # end
+  # def example_payload_request(of_format:)
+  #   {
+  #     payload: example_raw_payload(of_format: of_format),
+  #     user_agent: user_agent_for(of_format)
+  #   }
+  # end
+  # def job_request_data(type)
+  #   case type
+  #   when 'Git'
+  #     ['Bitbucket', 'git_job'] # develop branch payload
+  #   when 'JSON'
+  #     ['JSON', 'test_job']
+  #   when 'MinimalJob'
+  #     ['JSON', 'minimal_job']
+  #   when 'IgnoredBranch'
+  #     ['Github', 'git_job'] # master branch payload
+  #   when 'UnknownJob'
+  #     ['Bitbucket', 'unknown_job']
+  #   when 'EmptyJob'
+  #     ['JSON', 'empty_job']
+  #   when 'NonJSON'
+  #     ['NonJSON', 'test_job']
+  #   else
+  #     ['', '']
+  #   end
+  # end
+  # def example_job_request(of_type:)
+  #   format, job_name = job_request_data of_type
+  #   Oj.dump payload: example_raw_payload(of_format: format),
+  #           job_name: job_name,
+  #           user_agent: user_agent_for(format)
+  # end
 end
+
+
 
 # module JobHelper
 #   def example_job(in_context: {}, with_commands: [])
