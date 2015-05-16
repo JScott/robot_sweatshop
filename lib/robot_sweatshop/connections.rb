d327b976d79c50458e499190c784f22da93131ab
JScott <jvscott@gmail.com>
2015-05-15 22:22:22 -0700

    Cleaning up the lib helpers

---
+++ lib/robot_sweatshop/connections.rb
@@ -0,0 +1,11 @@
+require 'ezmq'
+
+# TODO: set up the client_settings and client stuff here
+# the current ezmq bug makes this hard so I'm waiting a bit first
+
+# server_settings = {
+#   port: configatron.payload_parser_port,
+#   encode: -> message { message },
+#   decode: -> message { Oj.load message }
+# }
+# server = EZMQ::Server.new server_settings
