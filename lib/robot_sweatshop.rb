f186368a509592c3fd4e572f23fdde928071ed89
JScott <jvscott@gmail.com>
2015-05-15 22:22:22 -0700

    Cleaning up the lib helpers

--- lib/robot_sweatshop.rb
+++ lib/robot_sweatshop.rb
@@ -1,5 +1,4 @@
-require_relative 'robot_sweatshop/moneta-queue'
-require_relative 'robot_sweatshop/queue-helper'
 require_relative 'robot_sweatshop/config'
 require_relative 'robot_sweatshop/payload'
 require_relative 'robot_sweatshop/cli'
+require_relative 'robot_sweatshop/connections'
