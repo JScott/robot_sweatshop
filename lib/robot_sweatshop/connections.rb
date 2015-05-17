854e6a7f5a2c01f6a5e15c9ffd2c72c83faea0c4
JScott <jvscott@gmail.com>
2015-05-17 16:37:57 -0700

    Gave up on being purely functional because the Assembler does too much at the moment and becomes less readable

--- lib/robot_sweatshop/connections.rb
+++ lib/robot_sweatshop/connections.rb
@@ -4,12 +4,9 @@ require 'robot_sweatshop/config'
 module ExtendedEZMQ
   refine EZMQ::Socket do
     def serialize_with_json!(except_encode_because_of_ezmq_bug: false)
+      # EZMQ issue #4
       self.encode = -> message { Oj.dump message } unless except_encode_because_of_ezmq_bug
       self.decode = -> message { Oj.load message }
     end
   end
-
-  # refine EZMQ do
-  #   def self.with
-  # end
 end
