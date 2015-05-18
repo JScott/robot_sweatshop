0400ee76cdcca3b11be0bd4ae412a24d7b27eeb2
JScott <jvscott@gmail.com>
2015-05-17 22:11:28 -0700

    Refining the spec a little

--- test/job_dictionary_spec.rb
+++ test/job_dictionary_spec.rb
@@ -5,7 +5,7 @@ require 'oj'
 require 'timeout'
 require 'robot_sweatshop/config'
 require_relative 'shared/setup'
-require_relative 'shared/helpers'
+require_relative 'shared/helpers' # do I need this?
 $stdout.sync = true
 
 Kintama.on_start do
@@ -23,18 +23,20 @@ describe 'the Job Dictionary' do
     @client = Setup::client port: configatron.job_dictionary_port
   end
 
-  %w(empty_job git_job minimal_job test_job).each do |job_name|
-    given "the '#{job_name}' job" do
+  %w(git_job minimal_job test_job).each do |job_name|
+    given "some #{job_name.gsub '_', ' '}" do
       setup do
         @response = Timeout.timeout($a_while) do
           @client.request job_name
         end
       end
 
-      should 'return a defined job object' do
+      should 'return no error' do
         assert_equal true, @response[:error].empty?
-        assert_kind_of Hash, @response[:payload]
+      end
 
+      should 'return a defined job object' do
+        assert_kind_of Hash, @response[:payload]
         no_environment = job_name =~ /empty|minimal/
         has_whitelist = job_name =~ /git/
         assert_not_nil @response[:payload]['branch_whitelist'] if has_whitelist
@@ -44,16 +46,18 @@ describe 'the Job Dictionary' do
     end
   end
 
-  given "an unknown job" do
-    setup do
-      @response = Timeout.timeout($a_while) do
-        @client.request 'not_a_job'
+  %w(empty_job undefined_job weird_job).each do |job_name|
+    given "some #{job_name.gsub '_', ' '}" do
+      setup do
+        @response = Timeout.timeout($a_while) do
+          @client.request 'not_a_job'
+        end
       end
-    end
 
-    should 'return an error object' do
-      assert_kind_of String, @response[:error]
-      assert_equal true, @response[:payload].empty?
+      should 'return an error object' do
+        assert_kind_of String, @response[:error]
+        assert_equal true, @response[:payload].empty?
+      end
     end
   end
 end
