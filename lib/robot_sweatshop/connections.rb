e55fad723a20fe8a255b9564da0fc334f1e7b0bf
JScott <jvscott@gmail.com>
2015-05-22 17:10:00 -0700

    Without that bug fix, we can simplify the Socket refine

--- lib/robot_sweatshop/connections.rb
+++ lib/robot_sweatshop/connections.rb
@@ -4,9 +4,8 @@ require 'robot_sweatshop/config'
 
 module ExtendedEZMQ
   refine EZMQ::Socket do
-    def serialize_with_json!(except_encode_because_of_ezmq_bug: false)
-      # EZMQ issue #4
-      self.encode = -> message { Oj.dump message } unless except_encode_because_of_ezmq_bug
+    def serialize_with_json!
+      self.encode = -> message { Oj.dump message }
       self.decode = -> message { Oj.load message }
     end
 
