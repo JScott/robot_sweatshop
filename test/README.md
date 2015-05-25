faecb53e97b7b078f2d847db3bdc723fb7f86905
JScott <jvscott@gmail.com>
2015-05-25 11:57:45 -0700

    New way to stub processes, only working on Input right now and will break everything else

--- test/README.md
+++ test/README.md
@@ -1,1 +1,3 @@
 You can run individual `_spec.rb` files with Ruby or `run_all.rb` to go through each.
+
+Because I'm lazy, TestProcess::Stub uses the UNIX `lsof` to check if ports are in use. There's probably a better, more portable way to do this.
--- test/shared/scaffolding.rb
+++ test/shared/scaffolding.rb
@@ -6,7 +6,19 @@ require 'robot_sweatshop/config'
 require 'robot_sweatshop/connections'
 using ExtendedEZMQ
 
-module Processes
+module Setup
+  def self.empty_conveyor
+    db_file = "#{configatron.database_path}/conveyor.db"
+    File.truncate db_file, 0 if File.exist? db_file
+  end
+
+  def self.populate_test_jobs
+    test_jobs = Dir.glob "#{__dir__}/../data/*_job.yaml"
+    test_jobs.each { |test_job| FileUtils.cp test_job, configatron.job_path }
+  end
+end
+
+module TestProcess
   def self.start(name_list)
     pids = []
     name_list.each do |name|
@@ -20,17 +32,32 @@ module Processes
   def self.stop(pid_list)
     pid_list.each { |pid| Process.kill 'TERM', pid }
   end
-end
 
-module Setup
-  def self.empty_conveyor
-    db_file = "#{configatron.database_path}/conveyor.db"
-    File.truncate db_file, 0 if File.exist? db_file
-  end
+  class Stub
+    def initialize(type, on_port:)
+      @type = type
+      @port = on_port
+      clear_output
+      @thread = Thread.new { create_listener type, on_port } unless running?
+    end
 
-  def self.stub(type, port:)
-    FileUtils.rm '.test.txt' if File.exist? '.test.txt'
-    Thread.new do
+    def running?
+      not `lsof -i :#{@port}`.empty?
+    end
+
+    def output_file
+      ".test.#{@type}"
+    end
+
+    def clear_output
+      FileUtils.rm output_file if File.exist? output_file
+    end
+
+    def output_empty?
+      (not File.exist? output_file) || File.read(output_file).empty?
+    end
+
+    def create_listener(type, port)
       listener = case type
       when 'Puller'
         EZMQ::Puller.new :connect, port: port
@@ -40,16 +67,16 @@ module Setup
       listener.serialize_with_json!
       listener.listen { |message| write message }
     end
-  end
 
-  def self.write(message)
-    file = File.new '.test.txt', 'w'
-    file.write message
-    file.close
-  end
+    def write(message)
+      file = File.new output_file, 'w'
+      file.write message
+      file.close
+    end
 
-  def self.populate_test_jobs
-    test_jobs = Dir.glob "#{__dir__}/../data/*_job.yaml"
-    test_jobs.each { |test_job| FileUtils.cp test_job, configatron.job_path }
+    def kill
+      @thread.kill
+      # loop while @thread.alive?
+    end
   end
 end
