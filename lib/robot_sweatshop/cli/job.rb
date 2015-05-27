844016878df06dca9d4198d8560faaff1cac9f0b
JScott <jvscott@gmail.com>
2015-05-27 14:47:55 -0700

    Adding the job-list method to the CLI

--- lib/robot_sweatshop/cli/job.rb
+++ lib/robot_sweatshop/cli/job.rb
@@ -10,9 +10,9 @@ module CLI
       "#{configatron.job_path}/#{job}.yaml"
     end
 
-    # def self.list
-    #   job_path = File.expand_path configatron.job_path
-    #   puts Dir.glob("#{job_path}/*.yaml").map { |file| File.basename(file, '.yaml') }
-    # end
+    def self.list
+      job_path = File.expand_path configatron.job_path
+      puts Dir.glob("#{job_path}/*.yaml").map { |file| File.basename(file, '.yaml') }
+    end
   end
 end
