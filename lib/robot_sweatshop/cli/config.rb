1927939ff70d2020a7180545469fff41883799db
JScott <jvscott@gmail.com>
2015-06-15 09:49:35 -0700

    Moved those methods and compiled to the user's home location

--- lib/robot_sweatshop/cli/config.rb
+++ lib/robot_sweatshop/cli/config.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require 'robot_sweatshop/config'
 
 module CLI
@@ -16,6 +17,24 @@ module CLI
       else
         '.robot_sweatshop/config.yaml'
       end
+    end
+
+    def self.expand_paths_in(config_hash)
+      config_hash.each do |key, value|
+        config_hash[key] = File.expand_path value if key.to_s.match(/_path/)
+      end
+    end
+
+    def self.write(config_hash)
+      config_home = File.expand_path('~/.robot_sweatshop')
+      FileUtils.mkpath config_home
+      File.write "#{config_home}/compiled_config.yaml", config_hash.to_yaml
+    end
+
+    def self.compile_to_file
+      config = expand_paths_in configatron.to_h
+      config[:working_path] = Dir.pwd
+      write config
     end
   end
 end
--- lib/robot_sweatshop/cli/start.rb
+++ lib/robot_sweatshop/cli/start.rb
@@ -5,20 +5,8 @@ require 'robot_sweatshop/config'
 module CLI
   # Methods for starting Robot Sweatshop
   module Start
-    def self.expand_paths_in(config_hash)
-      config_hash.each do |key, value|
-        config_hash[key] = File.expand_path value if key.to_s.match(/_path/)
-      end
-    end
-
-    def self.store_config_for_eye
-      config = expand_paths_in configatron.to_h
-      config[:working_path] = Dir.pwd
-      File.write '/tmp/.robot_sweatshop-eye-config.yaml', config.to_yaml
-    end
-
     def self.sweatshop(for_environment:)
-      store_config_for_eye
+      Config.compile_to_file
       eye_config = File.expand_path "#{__dir__}/../../../robot_sweatshop.eye"
       output = `eye load #{eye_config}`
       fail output if $?.exitstatus != 0
