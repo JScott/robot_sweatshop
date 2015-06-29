baf458675a8b1ac6a99a8500af1557abf6ea5023
JScott <jvscott@gmail.com>
2015-06-29 15:45:04 -0700

    Green - Overseer job form

--- lib/robot_sweatshop/overseer.rb
+++ lib/robot_sweatshop/overseer.rb
@@ -3,11 +3,20 @@ require 'erubis'
 # Helper methods for the Overseer Sinatra server
 module OverseerHelper
   def log_list
-    %w(assembler conveyor input job-dictionary overseer payload-parser worker)
+    %w(assembler conveyor input job-dictionary payload-parser worker)
+  end
+
+  def job_list
+    log_path = File.expand_path "#{configatron.job_path}"
+    p log_path, Dir.glob("#{log_path}/*.yaml")
+    Dir.glob("#{log_path}/*.yaml").map { |path| File.basename path, '.yaml' }
   end
 
   def frontpage
-    context = { logs: log_list }
+    context = {
+      jobs: job_list,
+      logs: log_list
+    }
     template = File.read "#{__dir__}/templates/index.html.eruby"
     eruby = Erubis::Eruby.new template
     eruby.result context
