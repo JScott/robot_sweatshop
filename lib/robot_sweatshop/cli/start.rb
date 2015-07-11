c174ca2093e8c7f8c2c8ae99700840e0e4fe9039
JScott <jvscott@gmail.com>
2015-07-10 18:57:17 -0700

    Cutting out command line cruft

--- lib/robot_sweatshop/cli/start.rb
+++ lib/robot_sweatshop/cli/start.rb
@@ -5,12 +5,12 @@ require 'robot_sweatshop/config'
 module CLI
   # Methods for starting Robot Sweatshop
   module Start
-    def self.sweatshop(for_environment:)
+    def self.sweatshop
       Config.compile_to_file
       eye_config = File.expand_path "#{__dir__}/../../../robot_sweatshop.eye"
       output = `eye load #{eye_config}`
       fail output if $?.exitstatus != 0
-      Announce.success "Robot Sweatshop loaded with a #{for_environment} configuration"
+      Announce.success "Robot Sweatshop loaded"
       Announce.info `eye restart robot_sweatshop`
       Announce.info 'Run \'eye --help\' for more info on debugging'
     end
