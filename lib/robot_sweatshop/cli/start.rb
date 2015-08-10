0077d14e96fd27225785e01409548643d3868306
JScott <jvscott@gmail.com>
2015-08-10 10:48:48 -0700

    Multiple workers based on the config

--- lib/robot_sweatshop/cli/start.rb
+++ lib/robot_sweatshop/cli/start.rb
@@ -19,7 +19,7 @@ module CLI
     def self.generate_eye_config(worker_count)
       template = File.read "#{__dir__}/robot_sweatshop.eye.erb"
       eruby = Erubis::Eruby.new template
-      compiled_config = eruby.result worker_count: worker_count
+      compiled_config = eruby.result worker_count: configatron.worker_count
       compiled_config_file = File.expand_path '~/.robot_sweatshop/robot_sweatshop.eye'
       File.open compiled_config_file, 'w' do |file|
         file.write compiled_config
