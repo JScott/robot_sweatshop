b25dda280b54df9343cee1b67f168865fb2b73f4
JScott <jvscott@gmail.com>
2015-08-10 09:31:31 -0700

    Dynamic generation of workers

--- lib/robot_sweatshop/cli/start.rb
+++ lib/robot_sweatshop/cli/start.rb
@@ -1,18 +1,29 @@
 require 'yaml'
 require 'terminal-announce'
 require 'robot_sweatshop/config'
+require 'erubis'
 
 module CLI
   # Methods for starting Robot Sweatshop
   module Start
-    def self.sweatshop
+    def self.sweatshop(with_worker_count: 1)
       Config.compile_to_file
-      eye_config = File.expand_path "#{__dir__}/../../../robot_sweatshop.eye"
+      eye_config = generate_eye_config with_worker_count
       output = `eye load #{eye_config}`
       fail output if $?.exitstatus != 0
       Announce.success "Robot Sweatshop loaded"
       Announce.info `eye restart robot_sweatshop`
       Announce.info 'Run \'eye --help\' for more info on debugging'
+    end
+
+    def self.generate_eye_config(worker_count)
+      eruby = Erubis::Eruby.new "#{__dir__}/robot_sweatshop.eye.erb"
+      compiled_config = eruby.result worker_count: worker_count
+      compiled_config_file = File.expand_path '~/.robot_sweatshop/robot_sweatshop.eye'
+      File.open compiled_config_file, 'w' do |file|
+        file.write compiled_config
+        file.path
+      end
     end
   end
 end
